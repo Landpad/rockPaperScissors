e36c73e80e4e0f917a41833f5403f3a2c54b23e7
landpad <20844115+Landpad@users.noreply.github.com>
2024-02-08 05:45:20 -0300

    Added style to the game

--- script.js
+++ script.js
@@ -1,123 +1,3 @@
-/* Need to create a function 'getComputerChoice' that
-* will return rock, paper or scissors.
-* Next need to write a function that plays a round of
-* rock paper scissors. Need to take playerSelection,
-* computerSelection AND then return a string that
-* declares the winner of the round
-* playerSelection parameter must be case-insensitive
-* if tie need to replay the round
-* create a function 'game' that plays 5 rounds
-* in a row AND determines the winner.
-* input need to be prompt
-* 1) create getComputerChoice function. This will return a rANDom choice between rock paper or scissors.
-* 2) create getPlayerChoice function. This will return rock paper or scissors.
-*       a) need to be case insensitive, the input will be transformed to capital case to solve this
-* 3) create the playRound function. This will return the winner of the round
-*       a) if the round end on tie. Need to be replayed
-* 4) create game function. This will play 5 rounds AND determine the winner of the 5.
-*       
-* FUNCTION getComputerChoice
-*   SET randomValue to random value between 1 AND 3
-*   SET weaponSelected to NULL
-*   CASE randomValue equals 1 THEN
-*       SET weaponSelected to 'Rock'
-*   END CASE
-*   CASE randomValue equals 2 THEN
-*       SET weaponSelected to 'Paper'
-*   END CASE
-*   CASE randomValue equals 3 THEN
-*       SET weaponSelected to 'Scissors'
-*   END CASE
-*   RETURN weaponSelected
-* END FUNCTION
-*
-* FUNCTION getPlayerChoice
-*   SET INPUT to UPPERCASE
-*   SET weaponSelected to NULL
-*   CASE INPUT equals 'ROCK' THEN
-*       SET weaponSelected to 'Rock'
-*   END CASE
-*   CASE INPUT equals 'PAPER' THEN 
-*       SET weaponSelected to 'Paper'
-*   END CASE
-*   CASE INPUT equals 'SCISSORS' THEN 
-*       SET weaponSelected to 'Scissors'
-*   END CASE
-*   CASE INPUT not equals 'ROCK' OR 'PAPER' OR 'SCISSORS' 
-*       THEN PRINT 'Input must be Rock, Paper or Scissors'
-*   END CASE
-*   RETURN weaponSelected
-* END FUNCTION
-*
-* FUNCTION playRound
-*   SET computerChoice TO INVOKE getComputerChoice
-*   SET playerChoice TO INVOKE getPlayerChoice
-*   SET playerWon TO null
-*   WHILE playerWon doesn't equals null THEN
-*   CASE computerChoice equals 'Rock' AND playerChoice equals 'Rock' THEN
-*       SET playerWon TO null
-*   END CASE
-*   CASE computerChoice equals 'Rock' AND playerChoice equals 'Paper' THEN
-*       SET tie TO false
-*       SET playerWon TO true
-*       PRINT 'You win!'
-*   END CASE
-*   CASE computerChoice equals 'Rock' AND playerChoice equals 'Scissors' THEN
-*       SET tie TO false
-*       SET playerWon TO false
-*       PRINT 'Computer wins!'
-*   END CASE
-*   CASE computerChoice equals 'Paper' AND playerChoice equals 'Paper' THEN
-*       SET playerWon TO null
-*   END CASE
-*   CASE computerChoice equals 'Paper' AND playerChoice equals 'Scissors' THEN
-*       SET tie TO false
-*       SET playerWon TO true
-*       PRINT 'You win!'
-*   END CASE
-*   CASE computerChoice equals 'Scissors' AND playerChoice equals 'Scissors' THEN
-*       SET playerWon TO null
-*   END CASE
-*   CASE computerChoice equals 'Scissors' AND playerChoice equals 'Rock' THEN
-*       SET tie TO false
-*       SET playerWon TO true
-*       PRINT 'You win!'
-*   END CASE
-*   CASE computerChoice equals 'Paper' AND playerChoice equals 'Rock' THEN
-*       SET tie TO false
-*       SET playerWon TO false
-*       PRINT 'Computer wins!'
-*   END CASE
-*   CASE computerChoice equals 'Scissors' AND playerChoice equals 'Paper' THEN
-*       SET tie TO false
-*       SET playerWon TO false
-*       PRINT 'Computer wins!'
-*   END CASE
-*   RETURN playerWon
-*   END WHILE
-* END FUNCTION
-*
-* FUNCTION playGame
-*   SET playerCounter to 0
-*   SET computerCounter to 0
-*   SET playerWon TO NULL
-*   WHILE (playerCounter less than 4 OR computerCounter less than 4) THEN
-*       SET playerWon TO INVOKE playRound
-*       CASE playerWon is true THEN
-*           SET playerCounter TO playerCounter + 1 ELSE
-*           SET computerCounter TO computerCounter + 1
-*       END CASE
-*   END WHILE
-*   CASE playerCounter is greater than computerCounter THEN
-*       SET playerWon to true
-*       PRINT 'Player won the game!' ELSE
-*       SET playerWon to false
-*       PRINT 'Computer won the game!'
-*   END CASE
-* END FUNCTION
-*   
-*/
-
 function getComputerChoice(value){
     let weaponSelected;
 
@@ -232,7 +112,7 @@ function getElement(id){
 
 }
 
-function disableRpsButtons(boolean){
+function toggleRpsButtons(boolean){
     const buttonsContainer = getElement('container');
     const buttons = buttonsContainer.querySelectorAll('button');
 
@@ -255,7 +135,22 @@ function displayPlayAgainButton(){
 
 }
 
+function restartGame(){
+    const playAgainButton = getElement('playAgainButton')
+    playAgainButton.addEventListener('click', () => {
+    const playAgainButton = getElement('playAgainButton');
 
+    playerScore = 0;
+    computerScore = 0;
+
+    toggleRpsButtons(false);
+    playAgainButton.remove();
+    
+    const body = document.querySelector('body');
+    location.reload();
+})
+
+}
 
 function playRound(selection){
     const roundPara = getElement('roundPara');
@@ -276,12 +171,17 @@ function playRound(selection){
     }
 
     if(gameFinished == true){
-        disableRpsButtons('false');
+        toggleRpsButtons(true);
         displayPlayAgainButton();
-        
+        restartGame();        
     }
 
     return winner;
+
+}
+
+function rpsEventListener(element, weaponSelected){
+    element.addEventListener('click', () => playRound(weaponSelected));
 
 }
 
@@ -289,13 +189,20 @@ let playerScore = 0;
 let computerScore = 0;
 
 const rockBtn = getElement('rock');
-rockBtn.addEventListener('click', () => playRound('ROCK'));
+rpsEventListener(rockBtn,'ROCK');
 
 const paperBtn = getElement('paper');
-paperBtn.addEventListener('click', () => playRound('PAPER'));
+rpsEventListener(paperBtn,'PAPER');
 
 const scissorsBtn = getElement('scissors');
-scissorsBtn.addEventListener('click', () => playRound('SCISSORS'));
+rpsEventListener(scissorsBtn,'SCISSORS');
+
+
+
+
+
+
+
 
 
 
